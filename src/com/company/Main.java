c9b7c5b3fa0746d88b61bbc0ca574905a0285a7a
andresandin <33686541+andresandin@users.noreply.github.com>
2019-04-09 14:52:14 +0200

    majs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    //inte test
-        System.out.println("Hello world!");
+        System.out.println("Hello world! majs");
     }
 }
