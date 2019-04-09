2f7c3aaf81bf874466ce6a14bdbfccc65d122d59
antonsjostrand <anton.sjostrand@hotmail.se>
2019-04-09 14:52:12 +0200

    test1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	    //inte test
-        System.out.println("Hello world!");
+<<<<<<< HEAD
+        System.out.println("Hello world! test");
+
+=======
+        System.out.println("Hello world! majs");
+>>>>>>> c9b7c5b3fa0746d88b61bbc0ca574905a0285a7a
     }
 }
